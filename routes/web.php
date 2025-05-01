f19fa9350eb8d26b7747d603adcdae9f9c12d8bb
Fotie Eric <ericfotie13@gmail.com>
2025-05-01 16:12:01 +0200

    template et controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Acceuilcontroller;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\site\Acceuilcontroller as SiteAcceuilcontroller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -29,3 +28,9 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+
+
+Route::get('/', [Acceuilcontroller::class, 'index'])->name('acceuil');
+
